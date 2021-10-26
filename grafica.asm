e62d0880e85270f7e715355ce9970a966370619d
Daniel Santos <danielsantosg951@gmail.com>
2021-10-26 08:55:10 -0600

    Agregando archivos para hacer refactor del codigo

---
+++ grafica.asm
@@ -0,0 +1,2 @@
+	; Todo el codigo referente a la grafica, ponerlo aqui
+	; excepto algunas macros que sean de uso general. Dejarlas en macros.asm
---
+++ operar.asm
@@ -0,0 +1,2 @@
+	; todo el codigo referente a las operaciones integrar/ derivar /resolver 
+	; excepto algunas macros de uso general, que se queden en macros.asm
